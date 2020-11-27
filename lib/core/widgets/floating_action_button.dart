a53ecc2588add822f0b6f3bdbb1df8ac0fb011ea
Lukas Jan <lukasjanek99@gmail.com>
2020-11-27 15:38:58 +0100

    ✨ Add some widgets

---
+++ lib/core/widgets/floating_action_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CustomFloatingActionButton extends StatelessWidget {
+  final Function onTap;
+
+  CustomFloatingActionButton(this.onTap);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 82,
+      height: 82,
+      child: FloatingActionButton(
+        backgroundColor: Colors.transparent,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(82),
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Get.theme.primaryColor,
+                Get.theme.accentColor,
+              ],
+            ),
+          ),
+          width: Get.width,
+          height: Get.height,
+          child: Icon(
+            Icons.add_rounded,
+            color: Colors.white,
+            size: 48,
+          ),
+        ),
+        onPressed: () {
+          //TODO: Bottom sheet with text field to adding our value
+        },
+      ),
+    );
+  }
+}
