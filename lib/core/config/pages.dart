875690fd99c3efc8b0aa425cf1a8e1c95d7628fb
Lukas Jan <lukasjanek99@gmail.com>
2020-11-27 19:29:08 +0100

    ✨ Add all values view

--- lib/core/config/pages.dart
+++ lib/core/config/pages.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:netguru_values/views/main/main_view_animation.dart';
+import 'package:netguru_values/views/main/main_view_controller.dart';
 import 'package:netguru_values/views/splash/splash_view_controller.dart';
+import 'package:netguru_values/views/values/values_view_controller.dart';
 
 import '../../views/favorites/favorites_view.dart';
 import '../../views/main/main_view.dart';
@@ -15,8 +19,22 @@ class Pages {
       page: () => SplashView(),
       binding: BindingsBuilder.put(() => SplashController()),
     ),
-    GetPage(name: RoutesName.MAIN, page: () => MainView()),
+    GetPage(
+      name: RoutesName.MAIN,
+      page: () => MainView(),
+      bindings: [
+        BindingsBuilder.put(() => MainAnimationController()),
+        BindingsBuilder.put(() => MainController()),
+      ],
+    ),
     GetPage(name: RoutesName.FAVORITES, page: () => FavoritesView()),
-    GetPage(name: RoutesName.VALUES, page: () => ValuesView()),
+    GetPage(
+      name: RoutesName.VALUES,
+      page: () => ValuesView(),
+      transition: Transition.fade,
+      curve: Curves.fastOutSlowIn,
+      transitionDuration: Duration(milliseconds: 375),
+      binding: BindingsBuilder.put(() => ValuesController()),
+    ),
   ];
 }
