0b9176c4f4324b44b41bcc24e40b186b0ccec001
Lukas Jan <lukasjanek99@gmail.com>
2020-11-27 15:39:40 +0100

    ✨ Add default values data

---
+++ lib/core/data/default_values.dart
@@ -0,0 +1,14 @@
+import 'package:netguru_values/core/models/value.dart';
+
+final List<ValueModel> defaultValue = [
+  ValueModel("Exceed clients' and colleagues' expectations"),
+  ValueModel(
+      "Take ownership and question the status quo in a constructive manner"),
+  ValueModel(
+      "Be brave, curious and experiment. Learn from all successes and failures"),
+  ValueModel("Act in a way that makes all of us proud"),
+  ValueModel(
+      "Build an inclusive, transparent and socially responsible culture"),
+  ValueModel("Be ambitious, grow yourself and the people around you"),
+  ValueModel("Recognize excellence and engagement"),
+];
